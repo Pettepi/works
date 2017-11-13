85aae68cb0f913489bdf3c7206dca0b8f29bf10c
Pettepi <33019301+Pettepi@users.noreply.github.com>
2017-11-13 13:04:58 +0200

    Add files via upload

---
+++ WebPer1-master/js/lomake.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "<p>".$_GET["name"]."</p>";
+echo "<p>".$_GET["surname"]."</p>";
+echo "<p>".$_GET["email"]."</p>";
+echo "<p>".$_GET["website"]."</p>";
+echo "<p>".$_GET["address"]."</p>";
+echo "<p>".$_GET["postalnumber"]."</p>";
+echo "<p>".$_GET["phonenumber"]."</p>";
+echo "<p>".$_GET["password"]."</p>";
+
+
+
+
+
+
+/**
+ * Created by PhpStorm.
+ * User: pettepi
+ * Date: 30.10.2017
+ * Time: 11.11
+ */
